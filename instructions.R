2d9f7f7fb3713ded7a1c8994dd46a44e95a2e046
Matlala Sefale <msefa24@stlawu.edu>
2026-04-13 14:22:18 -0400

    Added a to do list to help me keep track of my progress and set goals

--- instructions.R
+++ instructions.R
@@ -1,7 +1,19 @@
 - dont push data into repo. add it to gitignore
 - make data folder for data, add csv into it, then add ---  data/ in gitignore
-..already added the line to gitignore
+..don't need to do this because didn't have to download the data file
 
 
 - make consistent commits and pushes
 - make purposeful commit messages
+
+- can use AI if you cite the prompt and what you got from it -- eg for tweaking data
+- use claude for code
+- prompts can be in separate file
+
+TO DO:
+  - include oen or 2 visualizations on the readme file
+
+  - aim for 3 times the size of a regular project
+
+  - UPDATE BLOG WITH POST ABOUT FINAL PROJECT  
+You can copy-paste items from your full report to create this, but it should definitely be shorter than your full report. Try to aim for an approximate reading time of 5 minutes and include only what you think is most interesting about your project.
