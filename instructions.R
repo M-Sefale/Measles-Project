a755f1e316eedd85e3da8d5d653ab412a832a1a9
Matlala Sefale <msefa24@stlawu.edu>
2026-04-13 13:14:50 -0400

    Read in the data and added outline of the plan for the project including questions to answer and visualizations to consider making

---
+++ instructions.R
@@ -0,0 +1,7 @@
+- dont push data into repo. add it to gitignore
+- make data folder for data, add csv into it, then add ---  data/ in gitignore
+..already added the line to gitignore
+
+
+- make consistent commits and pushes
+- make purposeful commit messages
